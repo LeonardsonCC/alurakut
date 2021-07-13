b1708792ee999d0822425c33d4d9e3200575378d
LeonardsonCC <leonardson.carvalho@gmail.com>
2021-07-13 20:36:08 -0300

    Limitador na lista

--- src/components/BoxList/index.js
+++ src/components/BoxList/index.js
@@ -1,13 +1,16 @@
 import { ProfileRelationsBoxWrapper } from "../ProfileRelations";
 
-const BoxList = ({list, title}) => {
+const BoxList = ({ list, title }) => {
+  const isGreaterThanMax = list.length > 6;
+  const renderList = isGreaterThanMax ? list.slice(0, 6) : [...list];
+
   return (
     <ProfileRelationsBoxWrapper>
       <h2 className="smallTitle">
         {title} ({list.length})
       </h2>
       <ul>
-        {list.map((item) => (
+        {renderList.map((item) => (
           <li>
             <a href={item.link} key={item.id} target="_blank">
               <img src={item.image} />
@@ -16,6 +19,7 @@ const BoxList = ({list, title}) => {
           </li>
         ))}
       </ul>
+      {isGreaterThanMax ? (<a className="boxLink" href="#">Ver todos</a>) : null}
     </ProfileRelationsBoxWrapper>
   );
 };
